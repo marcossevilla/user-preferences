202806fd33df32a6f60d3e262ed89879078d6ebf
Marcos Sevilla <marcossevilla14@gmail.com>
2019-07-03 09:14:30 -0600

    added drawer in home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_preferences/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   static final String routeName = 'home';
@@ -9,6 +10,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('User Preferences'),
       ),
+      drawer: _createMenu(context),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -18,6 +20,60 @@ class HomePage extends StatelessWidget {
           Divider(),
           Text('Username: '),
           Divider(),
+        ],
+      ),
+    );
+  }
+
+  Drawer _createMenu(BuildContext context) {
+    return Drawer(
+      elevation: 5.0,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/header.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.find_in_page,
+              color: Colors.blue,
+            ),
+            title: Text('Pages'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.party_mode,
+              color: Colors.blue,
+            ),
+            title: Text('Party Mode'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.people,
+              color: Colors.blue,
+            ),
+            title: Text('People'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            title: Text('Settings'),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, SettingsPage.routeName),
+          ),
         ],
       ),
     );
